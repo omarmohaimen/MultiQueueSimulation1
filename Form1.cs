3d5b758d69372df1e8b48fb663d4f8eff1b0fe1b
omar ahmed abdelmohimen <85372271+omarmohaimen@users.noreply.github.com>
2023-06-25 17:58:53 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MultiQueueModels;
+using MultiQueueTesting;
+using System.Runtime.InteropServices;
+
+namespace MultiQueueSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //SimulationSystem system = new SimulationSystem("C:/me/study/4th year/1st semster/modling/labs/Lab 2/Template_Students/MultiQueueSimulation/MultiQueueSimulation/TestCases/TestCase1.txt");
+
+            //textBox1.Text = system.NumberOfServers.ToString();
+
+        }
+        string OpenedFilePath;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //string OpenedFilePath;
+                //Open the browsed image and display it
+                OpenedFilePath = openFileDialog1.FileName;
+                SimulationSystem system = new SimulationSystem(OpenedFilePath);
+                textBox1.Text = system.NumberOfServers.ToString();
+                //ImageMatrix = ImageOperations.OpenImage(OpenedFilePath);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
